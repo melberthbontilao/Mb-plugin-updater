a1ca98374a71f22406176c1d551332986c6278e0
melberthbontilao <melberthbontilao@linksync.com>
2015-10-29 01:34:37 -0700

    Create plugin-updater.php
    - Add plugin-updater.php file

---
+++ plugin-updater.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Plugin updater
+Plugin URI: https://github.com/melberthbontilao/Plugin-updater
+Description: This is test plugin.
+Author: MelberthB
+Version: 1.0
+Author URI: https://github.com/melberthbontilao/Plugin-updater
+*/
+
+add_action('plugins_loaded', 'plugin_updater_init', 0);
+
+include_once('LinksynceparcelPluginUpdater.php');
+function plugin_updater_init() {
+	if ( is_admin() ) {
+		$updater_plugin = new LinksynceparcelPluginUpdater();
+		$updater_plugin->initPluginUpdater( __FILE__, 'melberthbontilao', 'Plugin-updater', '9c19becb8b7d75f3f27d78c0646978ce311ddc18' );
+	}
+}
