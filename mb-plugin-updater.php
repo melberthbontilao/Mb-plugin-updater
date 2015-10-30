5e0fc91cbc5468f790c858e593ec1edf6951328c
melberthbontilao <melberthbontilao@linksync.com>
2015-10-29 19:53:35 -0700

    Update mb-plugin-updater.php

--- mb-plugin-updater.php
+++ mb-plugin-updater.php
@@ -4,7 +4,7 @@ Plugin Name: MB Plugin updater
 Plugin URI: https://github.com/melberthbontilao/Mb-plugin-updater
 Description: This is test plugin to check if plugin update is working.
 Author: MelberthB
-Version: 1.3
+Version: 1.4
 Author URI: https://github.com/melberthbontilao/Mb-plugin-updater
 License: GPLv2 or later
 */
